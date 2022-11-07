e15a60057abed5f897bd822321bfca2d71397c4e
Jax Tan <jaxtan@JaxdeMBP.attlocal.net>
2022-11-07 15:20:02 -0800

    finish REST part

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,12 +1,35 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
-const createTuit = (req, res) => {}
-const findTuits  = (req, res) => {
-    res.send(tuits);
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    newTuit._id = (new Date()).getTime() + "";
+    tuits.push(newTuit);
+    res.json(newTuit);
 }
-const updateTuit = (req, res) => {}
-const deleteTuit = (req, res) => {}
+
+const findTuits  = (req, res) => {
+    res.json(tuits);
+}
+
+const updateTuit = (req, res) => {
+    const tuitIdToUpdate = parseInt(req.params.tid);
+    const updates = req.body;
+    const tuitUpdateIdx = tuits.findIndex((t) => t._id === tuitIdToUpdate);
+    tuits[tuitUpdateIdx] = {
+        ...tuits[tuitUpdateIdx],
+        ...updates
+    }
+    res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitIdToDelete = parseInt(req.params["tid"]);
+    tuits = tuits.filter((t) => t["_id"] !== tuitIdToDelete);
+    res.sendStatus(200);
+}
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
