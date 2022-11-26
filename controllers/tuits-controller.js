cbb28ba823380d2a17e110b3d4902fe9468acc44
JaxDoge <niftyjax@gmail.com>
2022-11-25 20:40:17 -0800

    fix delete button 404 problem

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -34,5 +34,5 @@ export default (app) => {
     app.post('/api/tuits', createTuit);
     app.get('/api/tuits', findTuits);
     app.put('/api/tuits/:tid', updateTuit);
-    app.delete('/api/tuits', deleteTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
 }
